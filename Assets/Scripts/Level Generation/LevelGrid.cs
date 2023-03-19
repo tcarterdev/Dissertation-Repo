676a1580e36bec7bdb06f5ee93be9fac24028c73
Tariq Carter <tcarterdev@outlook.com>
2023-03-19 11:45:09 +0000

    Level Generation: Chest and Boss Room Spawns

--- Assets/Scripts/Level Generation/LevelGrid.cs
+++ Assets/Scripts/Level Generation/LevelGrid.cs
@@ -12,9 +12,13 @@ public class LevelGrid : MonoBehaviour
     [Header("Room Prefabs")]
     [SerializeField] GameObject hubRoom;
     [SerializeField] GameObject[] standardRooms;
+    [SerializeField] GameObject bossRoom;
+    [SerializeField] GameObject[] chestRoomPrefabs;
 
     [Header("Generator Settings")]
     [Range(0, 100)] public int roomSpawnChance;
+    public int minChestRooms;
+    public int maxChestRooms;
 
     private void Start()
     {
@@ -24,7 +28,7 @@ public class LevelGrid : MonoBehaviour
     private void RoomSpawn(GameObject room, Vector2Int posInGrid)
     {
         Debug.Log(posInGrid);
-       GameObject newRoom = Instantiate(room);
+        GameObject newRoom = Instantiate(room);
         Vector3 roomWorldPos = new Vector3(posInGrid.x * cellSize, 0f, posInGrid.y * cellSize);
         newRoom.transform.position = roomWorldPos;
     }
@@ -35,16 +39,31 @@ public class LevelGrid : MonoBehaviour
 
     private void GenerateMap()
     {
+        //Spawn HUB Room
+        Vector2Int hubRoomPos = new Vector2Int(boolGrid.GetLength(0) / 2, boolGrid.GetLength(1) / 2);
+        RoomSpawn(hubRoom, hubRoomPos);
+        boolGrid[hubRoomPos.x, hubRoomPos.y] = true;
+
+        //Spawn Boss Room
+        Vector2Int bossRoomPos = GetSpawnPosition();
+        RoomSpawn(bossRoom, bossRoomPos);
+        boolGrid[bossRoomPos.x, bossRoomPos.y] = true;
+
+        //Spawn Chest Room
+        int numberOfChestRooms = Random.Range(minChestRooms, maxChestRooms + 1);
+        for (int chestRooms = 0; chestRooms < numberOfChestRooms; chestRooms++)
+        {
+            Vector2Int chestRoomPos = GetSpawnPosition();
+            RoomSpawn(chestRoomPrefabs[Random.Range(0, chestRoomPrefabs.Length)], chestRoomPos);
+            boolGrid[chestRoomPos.x, chestRoomPos.y] = true;
+        }
+
         for (int x = 0; x < boolGrid.GetLength(0); x++)
         {
             for (int y = 0; y < boolGrid.GetLength(1); y++)
             {
                 //Check if Hub Room is Center
-                if (x == boolGrid.GetLength(0) / 2 && y == boolGrid.GetLength(1) / 2)
-                {
-                    RoomSpawn(hubRoom, new Vector2Int(x,y));
-                }
-                else //Spawn Standard Room
+                if (boolGrid[x, y] != true)
                 {
                     int randnum = Random.Range(0, 100);
                     if (randnum <= roomSpawnChance)
@@ -52,33 +71,40 @@ public class LevelGrid : MonoBehaviour
                         GameObject roomToSpawn = standardRooms[Random.Range(0, standardRooms.Length)];
                         RoomSpawn(roomToSpawn, new Vector2Int(x, y));
                     }
-                   
+
                 }
             }
 
         }
     }
 
-    //private void ondrawgizmosselected()
-    //{
-    //    gridsetup();
+    public bool CanSpawnHere(Vector2Int spawnPos)
+    {
+        if (boolGrid[spawnPos.x, spawnPos.y] == false)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
 
-    //    for (int x = 0; x < boolgrid.getlength(0); x++)
-    //    {
-    //        for (int y = 0; y < boolgrid.getlength(0); y++)
-    //        {
-    //            if (boolgrid[x, y] == true)
-    //            {
-    //                gizmos.color = color.red;
+    }
 
-    //            }
-    //            else
-    //            {
-    //                gizmos.color = color.green;
-    //            }
-    //            vector3 cellpos = new vector3(x * cellsize, 0f, y * cellsize);
-    //            gizmos.drawsphere(cellpos, 5);
-    //        }
-    //    }
-    //}
+    public Vector2Int GetSpawnPosition()
+    {
+        Vector2Int bossRoomPos = new Vector2Int(Random.Range(0, boolGrid.GetLength(0)), Random.Range(0, boolGrid.GetLength(1)));
+        if (CanSpawnHere(bossRoomPos))
+        {
+            //boolGrid[bossRoomPos.x, bossRoomPos.y] = true;
+            return bossRoomPos;
+        }
+        else
+        {
+
+            return GetSpawnPosition();
+        }
+
+        
+    }
 }
